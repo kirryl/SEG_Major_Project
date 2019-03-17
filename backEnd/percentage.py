a04e10a5c5064bebe34c24ae547bdfb6c7605bc8
Diogo (Diogo) Ferreira Ramos <diogo.ramos@kcl.ac.uk>
2019-03-17 22:24:05 +0000

    Finalise phase 1 of matching

--- backEnd/percentage.py
+++ backEnd/percentage.py
@@ -31,73 +31,54 @@ mentorCursor.close()
 
 i = 0
 interestsCount =[]
-studentMentorPercentile = [][]
+#studentMentorPercentile = [[],[]]
+
+
+studentMentorPercentile = []
+for k in range(len(studentList)):
+    studentMentorPercentile.append([0] * len(mentorList))
+	
 
 while i < len(studentList):
 	j = 0
-	
+	mentorPercentile = []
 	while j < len(mentorList):
 		interestsName = []
 		interestsCursor = mydb.cursor()
 		interestsCursor.execute("SELECT InterestTitle FROM STUDENT_INTERESTS NATURAL JOIN MENTOR_INTERESTS WHERE StudentID="+str(i)+" AND MentorID="+str(j)+" AND STUDENT_INTERESTS.InterestTitle = MENTOR_INTERESTS.InterestTitle")		
 		interestsName.append(interestsCursor.fetchall())
-		matchingInterests = len(interestsName)
+		matchingInterests = len(interestsName[0])
 		#interestsCount.append(len(interestsCursor.fetchall()))
 		interestsCursor.close()
-		
-		interestsName = []
+		print(interestsName)
+		print("matching interests"+str(matchingInterests))
+		interestsNameTwo = []
 		interestsCursor = mydb.cursor()
-		interestsCursor.execute("SELECT a.firstInterest from(SELECT studentID, SIMILAR_INTERESTS.firstInterest FROM STUDENT_INTERESTS JOIN SIMILAR_INTERESTS WHERE studentID="+str(i)+" AND (STUDENT_INTERESTS.interestTitle=SIMILAR_INTERESTS.secondInterest)) as a JOIN MENTOR_INTERESTS WHERE mentorID="+str(j)+" AND (a.firstInterest=MENTOR_INTERESTS.interestTitle)")		
-		interestsName.append(interestsCursor.fetchall())
+		interestsCursor.execute("SELECT a.firstInterest from(SELECT StudentID, SIMILAR_INTERESTS.firstInterest FROM STUDENT_INTERESTS JOIN SIMILAR_INTERESTS WHERE StudentID="+str(i)+" AND (STUDENT_INTERESTS.interestTitle=SIMILAR_INTERESTS.secondInterest)) as a JOIN MENTOR_INTERESTS WHERE MentorID="+str(j)+" AND (a.firstInterest=MENTOR_INTERESTS.interestTitle)")		
+		interestsNameTwo.append(interestsCursor.fetchall())
 		interestsCursor.close()
 		interestsCursor = mydb.cursor()
-		interestsCursor.execute("SELECT a.secondInterest from (SELECT studentID, SIMILAR_INTERESTS.secondInterest FROM STUDENT_INTERESTS JOIN SIMILAR_INTERESTS WHERE studentID="+str(i)+" AND (STUDENT_INTERESTS.interestTitle=SIMILAR_INTERESTS.firstInterest)) as a JOIN MENTOR_INTERESTS WHERE mentorID="+str(j)+" AND (a.secondInterest=MENTOR_INTERESTS.interestTitle)")		
-		interestsName.append(interestsCursor.fetchall())
+		interestsCursor.execute("SELECT a.secondInterest from (SELECT StudentID, SIMILAR_INTERESTS.secondInterest FROM STUDENT_INTERESTS JOIN SIMILAR_INTERESTS WHERE StudentID="+str(i)+" AND (STUDENT_INTERESTS.interestTitle=SIMILAR_INTERESTS.firstInterest)) as a JOIN MENTOR_INTERESTS WHERE MentorID="+str(j)+" AND (a.secondInterest=MENTOR_INTERESTS.interestTitle)")		
+		interestsNameTwo.append(interestsCursor.fetchall())
 		interestsCursor.close()
-		similarInterests = len(interestsName)
+		similarInterests = len(interestsNameTwo[0])
+		print("similarInterests"+str(similarInterests))
 		
-		interestsName = []
+		interestsNameTotal = []
 		interestsCursor = mydb.cursor()
-		interestsCursor.execute("SELECT studentID from STUDENT_INTERESTS WHERE studentID="+str(i))		
-		interestsName.append(interestsCursor.fetchall())
-		totalInterests = len(interestsName)
-		interestsCursor.close()
+		interestsCursor.execute("SELECT InterestTitle FROM STUDENT_INTERESTS WHERE StudentID="+str(i)+"")		
+		interestsNameTotal.append(interestsCursor.fetchall())
+		totalInterests = len(interestsNameTotal[0])
+		interestsCursor.close()	
+		print("totalInterests"+str(totalInterests))
 		
-		studentMentorPercentile[i][j] = 100*(matchingInterests+0.5*similarInterests)/totalInterests
-		
+		#mentorPercentile.append(100*(matchingInterests+0.5*similarInterests)/totalInterests)		
+		studentMentorPercentile[i][j] = 100*(matchingInterests+0.5*similarInterests)/totalInterests		
 		j = j + 1
-	
+	#studentMentorPercentile[i].append(mentorPercentile)	
 	i = i + 1
 
-
-print(interestsName)
-
-
-
-initialStudentCursor = mydb.cursor()
-initialMentorCursor = mydb.cursor()
-cursorStudent = initialStudentCursor.execute("SELECT * FROM STUDENTS;")
-cursorMentor = initialMentorCursor.execute("SELECT * FROM MENTORS;")
-
-
-row = initialStudentCursor.fetchone()
-col = initialMentorCursor.fetchone()
-
-
-while row is not None:
-    print(row)
-    row = initialStudentCursor.fetchone()
-	while col is not None:
-		print(col)
-		col = initialMentorCursor.fetchone()
-
 	
-try:
-	with mydb.cursor() as cursor:
-		cursor.execute("SELECT * FROM STUDENTS;")
-finally:
-	connection.close()
+for k in range(len(studentList)):
+	print(studentMentorPercentile[k])
 
-	
-	
-#interestsCursor.execute("SELECT STUDENT_INTERESTS.InterestTitle FROM STUDENT_INTERESTS INNER JOIN MENTOR_INTERESTS ON StudentID="+str(i)+" AND MentorID="+str(j)+" AND STUDENT_INTERESTS.InterestTitle = MENTOR_INTERESTS.InterestTitle")
