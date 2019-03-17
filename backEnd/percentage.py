755e30d06db53b0a9899d5e86d9cdf8df2a566bb
Kirryl Tarunin <kirryltarunin@outlook.com>
2019-03-17 21:25:27 +0000

    Create percentile query

--- backEnd/percentage.py
+++ backEnd/percentage.py
@@ -47,21 +47,23 @@ while i < len(studentList):
 		
 		interestsName = []
 		interestsCursor = mydb.cursor()
-		interestsCursor.execute("SELECT firstInterest AND secondInterest FROM STUDENT_INTERESTS NATURAL JOIN MENTOR_INTERESTS WHERE StudentID="+str(i)+" AND MentorID="+str(j)+" AND STUDENT_INTERESTS.InterestTitle = MENTOR_INTERESTS.InterestTitle")		
+		interestsCursor.execute("SELECT a.firstInterest from(SELECT studentID, SIMILAR_INTERESTS.firstInterest FROM STUDENT_INTERESTS JOIN SIMILAR_INTERESTS WHERE studentID="+str(i)+" AND (STUDENT_INTERESTS.interestTitle=SIMILAR_INTERESTS.secondInterest)) as a JOIN MENTOR_INTERESTS WHERE mentorID="+str(j)+" AND (a.firstInterest=MENTOR_INTERESTS.interestTitle)")		
 		interestsName.append(interestsCursor.fetchall())
-		similarInterests = len(interestsCursor.fetchall())
-		#interestsCount.append(len(interestsCursor.fetchall()))
 		interestsCursor.close()
+		interestsCursor = mydb.cursor()
+		interestsCursor.execute("SELECT a.secondInterest from (SELECT studentID, SIMILAR_INTERESTS.secondInterest FROM STUDENT_INTERESTS JOIN SIMILAR_INTERESTS WHERE studentID="+str(i)+" AND (STUDENT_INTERESTS.interestTitle=SIMILAR_INTERESTS.firstInterest)) as a JOIN MENTOR_INTERESTS WHERE mentorID="+str(j)+" AND (a.secondInterest=MENTOR_INTERESTS.interestTitle)")		
+		interestsName.append(interestsCursor.fetchall())
+		interestsCursor.close()
+		similarInterests = len(interestsCursor.fetchall())
 		
 		interestsName = []
 		interestsCursor = mydb.cursor()
-		interestsCursor.execute("SELECT InterestTitle FROM STUDENT_INTERESTS NATURAL JOIN MENTOR_INTERESTS WHERE StudentID="+str(i)+" AND MentorID="+str(j)+" AND STUDENT_INTERESTS.InterestTitle = MENTOR_INTERESTS.InterestTitle")		
+		interestsCursor.execute("SELECT studentID from STUDENT_INTERESTS WHERE studentID="+str(i))		
 		interestsName.append(interestsCursor.fetchall())
 		totalInterests = len(interestsCursor.fetchall())
-		#interestsCount.append(len(interestsCursor.fetchall()))
 		interestsCursor.close()
 		
-		studentMentorPercentile[i][j] = 100*(matchingInterests+similarInterests)/totalInterests
+		studentMentorPercentile[i][j] = 100*(matchingInterests+0.5*similarInterests)/totalInterests
 		
 		j = j + 1
 	
