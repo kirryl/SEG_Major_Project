6a61177afd44bfb7bb54ba4f498c845464597f12
Mansour Jalaly <mansour.jalaly@kcl.ac.uk>
2019-03-23 19:34:27 +0000

    Add frontend into framework

--- project/templates/static/js/index.jsx
+++ project/templates/static/js/index.jsx
@@ -1,5 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import routes from "./routes";
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
 
-ReactDOM.render(routes, document.getElementById("content"));
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/scss/paper-dashboard.scss";
+import "assets/demo/demo.css";
+
+import indexRoutes from "routes/index.jsx";
+
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      {indexRoutes.map((prop, key) => {
+        return <Route path={prop.path} key={key} component={prop.component} />;
+      })}
+    </Switch>
+  </Router>,
+  document.getElementById("content")
+);
